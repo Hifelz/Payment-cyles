f902168ef63d63c08bb1b527d09ed2bb5106d5b4
Hifelz <100286956+Hifelz@users.noreply.github.com>
2023-05-15 15:18:52 -0300

    feat: billingCycleService.js

---
+++ src/api/billingCycle/billingCycleService.js
@@ -0,0 +1,5 @@
+const billingCycle = require('./billingCycle')
+BillingCycle.methods(['get', 'post', 'put', 'delete']}
+BillingCycle.updateOptions({new: true, runValidators: true})
+
+module.export = BillingCycle
