420cbf574ea2f1ac4ab465ae71f3fb0ea620ad18
Hifelz <100286956+Hifelz@users.noreply.github.com>
2023-05-15 15:30:25 -0300

    Update billingCycleService.js
    get was returning an empty array, so I decided to add a conditional state saying how 'get' should behave

--- src/api/billingCycle/billingCycleService.js
+++ src/api/billingCycle/billingCycleService.js
@@ -1,5 +1,19 @@
 const billingCycle = require('./billingCycle')
+
 BillingCycle.methods(['get', 'post', 'put', 'delete']}
+                     
 BillingCycle.updateOptions({new: true, runValidators: true})
+
+BillingCycle.route('get', (req, res, next) => {
+
+  BillingCycle.route('get', (req, res, next) => {
+    BillingCycle.find({}, (err, docs) => {
+        if(!err) {
+            res.json(docs)
+        } else {
+            res.status(500).json({errors: [error]})
+        }
+    })
+})
 
 module.export = BillingCycle
